9e687400e0339850f81bb37a6977533103348253
Louis Haywood <yaloumessenger@yahoo.com>
2017-01-29 00:57:07 -0800

    added comments to my js file

--- app.js
+++ app.js
@@ -1,76 +1,102 @@
 'use strict';
+// Prompt the user to input their name.
+// Store user's name in a variable.
+// Ask the user eight questions and take their answer as input which will be stored in a variable.
+var userName = prompt('What\'s your name?');
+alert('Hello ' + userName + '. Thanks for coming to my site!');
+console.log('The user name is: ' + userName);
 
-// var userName = prompt('What\'s your name?');
-// alert('Hello ' + userName + '. Thanks for coming to my site!');
-// console.log('The user name is: ' + userName);
-// // We want to ask the user a question
-// // Take their answer as input; stored in a var
-// // Validate that their answer matches my answer
-// // Depending on true or False, provide feedback
-// var answerOne = prompt('What city was I born in?').toUpperCase();
-// console.log('The answer to question one is: ' + answerOne);
-// var myAnswerOne = 'philadelphia';
-//
-// if(answerOne === myAnswerOne || 'philly') {
-//   alert('You got it right. I was born in Philly!');
-// } else {
-//   alert('Sorry, that\'s wrong. I was born in Philadelphia');
-// }
-//
-// var answerTwo = prompt('What brought me to Washington?').toUpperCase();
-// console.log('The answer to question two is: ' + answerTwo);
-// var myAnswerTwo = 'military';
-//
-// if(answerTwo === myAnswerTwo || 'navy') {
-//   alert('You are correct. I am a US Navy Vet and I was stationed in Bremerton WA!');
-// } else {
-//   alert('Ahh, that\'s incorrect. I am a US Navy Vet and I was stationed in Bremerton WA!');
-// }
-//
-// var answerThree = prompt('What business have I owned?').toUpperCase();
-// console.log('The answer to question three is: ' + answerThree);
-// var myAnswerThree = 'MOVING';
-//
-// if(answerThree === myAnswerThree) {
-//   alert('You are correct. I\'ve owned a moving company!');
-// } else {
-//   alert('Oh no, that\'s incorrect. I\'ve owned a moving company!');
-// }
-//
-// var answerFour = prompt('What profession have I spent most of my career in?').toLowerCase();
-// console.log('The answer to question four is: ' + answerFour);
-// var myAnswerFour = 'sales';
-//
-// if(answerFour === myAnswerFour) {
-//   alert('You are correct. I have a background in sales!');
-// } else {
-//   alert('Oh no, that\'s incorrect. I have a background in sales!');
-// }
-//
-// var answerFive = prompt('Am I a people person?').toLowerCase();
-// console.log('The answer to question five is: ' + answerFive);
-//
-// if(answerFive === 'y' || answerFive === 'yes') {
-//   alert('You are correct. I enjoy talking to people!');
-// } else {
-//   (answerFive === 'n' || answerFive === 'no');
-//   alert('Oh no, that\'s incorrect. I do enjoy talking to people!');
-// }
-// var answerSix = prompt('Can you guess what\'s my favorite food?');
-// console.log('The answer to question five is: ' + answerSix);
-//
-// var food = ['Grilled Salmon', 'Fried Chicken', 'Philly Cheesesteak', 'Pasta'];
-// food.indexOf('Grilled Salmon');
-// food.indexOf('Fried Chicken');
-// food.indexOf('Philly Cheesesteak');
-// food.indexOf('Pasta');
-//
-// if(food.indexOf('Grilled Salmon') === 0) {
-//   alert('I love it too however my favorite is Grilled Salmon.');
-// } else {
-//   alert('My favorite is Grilled Salmon but that\'s good too');
-// }
+// Question one.
+// Change user's input for the first question to all lower case.
+// Validate that their answer matches my answer or not.
+var answerOne = prompt('What city was I born in?').toLowerCase();
+console.log('The answer to question one is: ' + answerOne);
+var myAnswerOne = 'philadelphia';
+// User's input for correct answer can be philadelphia or philly.
+// Depending on true or False, provide feedback.
+if(answerOne === myAnswerOne || 'philly') {
+  alert('You got it right. I was born in Philly!');
+} else {
+  alert('Sorry, that\'s wrong. I was born in Philadelphia');
+}
 
+// Question two.
+// Change user's input for the second question to all lower case.
+// Validate that their answer matches my answer or not.
+var answerTwo = prompt('What brought me to Washington?').toLowerCase();
+console.log('The answer to question two is: ' + answerTwo);
+var myAnswerTwo = 'military';
+// User's input for correct answer can be military or navy.
+// Depending on true or False, provide feedback.
+if(answerTwo === myAnswerTwo || 'navy') {
+  alert('You are correct. I am a US Navy Vet and I was stationed in Bremerton WA!');
+} else {
+  alert('Ahh, that\'s incorrect. I am a US Navy Vet and I was stationed in Bremerton WA!');
+}
+
+// Question three.
+// Change user's input for the third question to all upper case.
+// Validate that their answer matches my answer or not.
+var answerThree = prompt('What business have I owned?').toUpperCase();
+console.log('The answer to question three is: ' + answerThree);
+var myAnswerThree = 'MOVING';
+// User's input for correct answer is military.
+// Depending on true or False, provide feedback.
+if(answerThree === myAnswerThree) {
+  alert('You are correct. I\'ve owned a moving company!');
+} else {
+  alert('Oh no, that\'s incorrect. I\'ve owned a moving company!');
+}
+
+// Question four.
+// Change user's input for the first question to all lower case.
+// Validate that their answer matches my answer or not.
+var answerFour = prompt('What profession have I spent most of my career in?').toLowerCase();
+console.log('The answer to question four is: ' + answerFour);
+var myAnswerFour = 'sales';
+// User's input for correct answer is sales.
+// Depending on true or False, provide feedback.
+if(answerFour === myAnswerFour) {
+  alert('You are correct. I have a background in sales!');
+} else {
+  alert('Oh no, that\'s incorrect. I have a background in sales!');
+}
+
+// Question five.
+// Change user's input for the fifth question to all lower case.
+// Validate that their answer matches my answer or not.
+var answerFive = prompt('Am I a people person?').toLowerCase();
+console.log('The answer to question five is: ' + answerFive);
+// User's input for correct answer can be y or yes.
+// Depending on true or False, provide feedback.
+if(answerFive === 'y' || answerFive === 'yes') {
+  alert('You are correct. I enjoy talking to people!');
+} else {
+  // User's input for correct answer can be n or no.
+  (answerFive === 'n' || answerFive === 'no');
+  alert('Oh no, that\'s incorrect. I do enjoy talking to people!');
+}
+
+// Question six.
+// Change user's input for the six question to all lower case.
+var answerSix = prompt('Can you guess what\'s my favorite food?');
+console.log('The answer to question five is: ' + answerSix);
+// Create an array and assign it to a variable.
+var food = ['grilled salmon', 'fried chicken', 'philly cheesesteak', 'pasta'];
+food.indexOf('grilled salmon');
+food.indexOf('fried chicken');
+food.indexOf('philly cheesesteak');
+food.indexOf('pasta');
+// Use the indexOf method to point out the correct answer in the array.
+if(food.indexOf('grilled salmon') === 0) {
+  alert('I love it too however my favorite is Grilled Salmon.');
+} else {
+  alert('My favorite is Grilled Salmon but that\'s good too');
+}
+
+// Question seven.
+// Use a do while loop to execute code to loop through a series of if else statments.
+// Start the loop at 0.
 var counter = 0;
 do {
   counter++;
@@ -80,28 +106,38 @@ do {
   if(answerSeven <= 10) {
     alert('That\'s too low aim a little higher.');
     console.log('The answer to question seven is: ' + answerSeven);
+    // Give feedback if answer is less than or equal to 10.
   }
   else if(answerSeven > 10 && answerSeven <= 15) {
-    alert('Try again, but aim alittle higher.');
+    alert('Try again, but aim a little higher.');
     console.log('The answer to question seven is: ' + answerSeven);
+    // Give feedback if answer is greater than 10 and less than or equal to 15.
   }
   else if(answerSeven > 15 && answerSeven <= 19) {
     alert('Your close, but no cigar. Guess again but just a wee bit higher.');
     console.log('The answer to question seven is: ' + answerSeven);
+    // Give feedback if answer is greater than 15 and less than or equal to 19.
   }
   else if(answerSeven == 20) {
     alert('Wow, you are correct. Good guess!');
+    // Use the keyword break to stop the loop once the correct input is evaluated.
     break;
+    // Give feedback if answer is equal to 20.
+    // Use double equals to except just the value of 20.
   }
+  // Run the loop four times.
 } while (counter < 4);
 
+// Question eight.
+// Use a for loop to loop through an array six times.
 for(var index = 0; index < 6; index++) {
   var places = ['singapore', 'maui', 'austrailia', 'dubai', 'hongkong'];
   var answerEight = prompt('What places have I traveled to?').toLowerCase();
   console.log('The answer to question eight is: ' + answerEight);
-//
+  // If user's input is not outside of the array.
   if (places.indexOf(answerEight) !== - 1) {
     alert('Yes, I\'ve been there and would love to go back someday.');
+    // Use the keyword break to stop the loop.
     break;
   }
   else {
